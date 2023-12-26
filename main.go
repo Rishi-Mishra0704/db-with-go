ea925da16169edc25c1498eb66cc614f9a249061
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-27 01:15:15 +0530

    add crud with gorm

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Name  string
+	Price uint
+}
+
+func main() {
+	dsn := "user=rishi dbname=test_gorm password=1111 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.AutoMigrate(&Product{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// CRUD Operations
+
+	// Create
+	product := Product{Name: "Laptop", Price: 1000}
+	product2 := Product{Name: "Mobile", Price: 500}
+	result := db.Create(&product)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	fmt.Printf("Created product: %+v\n", product)
+
+	result2 := db.Create(&product2)
+	if result2.Error != nil {
+		log.Fatal(result.Error)
+	}
+	fmt.Printf("Created product: %+v\n", product)
+	// Read
+	var fetchedProduct Product
+	result = db.First(&fetchedProduct, product.ID)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	fmt.Printf("Found product: %+v\n", fetchedProduct)
+
+	// Update
+	db.Model(&fetchedProduct).Update("Price", 1200)
+	fmt.Printf("Updated product: %+v\n", fetchedProduct)
+
+	// Delete
+	result = db.Delete(&fetchedProduct)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	fmt.Println("Deleted product:", fetchedProduct)
+
+}
